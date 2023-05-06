786e8d76057d45fd1f3487154ebd5e275709a9ec
Nurul Afifah Yardi <nurulafifahyardi2002@gmail.com>
2023-05-06 14:26:13 +0700

    Menambahkan projek

---
+++ kelompok_bootcamp/resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; {{ now()->year }} <div class="bullet"></div> Create by <a href=>bootcamp finals</a>
+    </div>
+    <div class="footer-right">
+        18.0.3
+    </div>
+</footer>
